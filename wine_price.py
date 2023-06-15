d961b8ddde66e78a697466480e670415cf8c5105
Rudra Kumar Shukla <83577844+RudraShukla-Code@users.noreply.github.com>
2023-06-15 17:03:15 +0530

    Update wine_price.py

--- wine_price.py
+++ wine_price.py
@@ -4,17 +4,7 @@ import pandas as pd
 import numpy as np
 import base64
 
-# open a file, where you stored the pickled data
-file = open('Wine_Price_Model.pkl', 'rb')
-
-# dump information to that file
-wine_predict = pickle.load(file)
-#wine_predict = pickle.load(open('Wine_Price_Model.pkl','rb'))
-wine_data = pd.read_csv('wine_file.csv')
-# close the file
-file.close()
-
-#wine_predict = pickle.load(open('Wine_Price_Model.pkl','rb'))
+wine_predict = pickle.load(open(' Wine_Price_Model.pkl','rb'))
 #wine_data = pd.read_csv('wine_file.csv')
 
 st.set_page_config(page_title="Wine Price Prediction App",layout="wide")
